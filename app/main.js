a021145cd092d1f5c858236a1fcba2d9c084a571
Brian Clifford <bcliff323@gmail.com>
2013-07-20 22:56:15 -0700

    Circular dependencies, single dependency

--- app/main.js
+++ app/main.js
@@ -1,36 +1,105 @@
 (function(window){
-	
-	window.app = {
-		modules : [],
-		require : function() {
-			var args = Array.prototype.slice.call(arguments);
-			var mod = args.pop().call();
+    
+    window.app = {
 
-			app.modules.push(mod);
-		}
-	};
+        modules : {},
+
+        module : function() {
+            var args = Array.prototype.slice.call(arguments);
+            var name = args[0];
+            var deps = args[1];
+            var mod = args.pop().call();
+
+            app.modules[name] = mod;
+            mod.name = name;
+            mod.deps = deps;
+
+            app.resolve(deps, name, mod);
+        },
+
+        resolve : function(deps, n, mod) {
+            if(mod.deps.length) {
+                var safe = false;
+
+                for (var i = 0, len = mod.deps.length; i < len; i++) {
+                    if(app.modules[mod.deps[i]] !== undefined) {
+                        app.modules[n].init();
+                    } else {
+                        domReady(function () {
+                            if(app.modules[mod.deps[i-1]] !== undefined) {
+                                app.modules[n].init();
+                            } else {
+                                if (window.console) {
+                                    console.log('[Error] Module: ' + app.modules[n].name + 
+                                    ' contains a reference to non-existent module - ' +
+                                        mod.deps[i-1] );
+                                }
+                            }
+                        }); 
+                    }
+                }
+            } else {
+                mod.init();
+            }
+        }
+
+    };
 
 })(window);
 
-	
-app.require(	
-	[
-		'foo',
-		'bar',
-		'dep'
-	],
+app.module('foo',    
+    [
+        'baz'
+    ],
 
-	function(msg) {
-		var self = {};
+    function() {
+        var root = app;
+        var self = {};
+        var baz = {};
 
-		self.name = 'baz';
+        self.init = function() {
+            console.log('init foo');
+        };
 
-		self.getName = function() {
-			return self.name;
-		};
+        return self;
+    }
+);
+    
+app.module('baz',   
+    [
+        'foo',
+        'that'
+    ],
 
-		return self;
-	}
+    function() {
+        var mods = app.modules;
+        var self = {};
+        var foo = {};
+
+        self.init = function() {
+            console.log('init baz');
+        };
+
+        return self;
+    }
 );
 
-console.log(app.modules);
+
+app.module('that',    
+    [
+        'foo',
+        'baz'
+    ],
+
+    function() {
+        var mods = app.modules;
+        var self = {};
+        var foo = {};
+
+        self.init = function() {
+            console.log('init that');
+        };
+
+        return self;
+    }
+);
